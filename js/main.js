c60cf675e3beabf616d370330e7d90ae9244e77d
Dean Hamann <hamann.dean@gmail.com>
2017-10-11 23:21:44 -0700

    Added Button logic / Cleaned up CSS styling

--- js/main.js
+++ js/main.js
@@ -1,44 +1,59 @@
 // $(function(){
 
-var deck =  [];
+var deck;
 
-var dealerHand = [];
-
-var playerHand = [];
+var dealerHand;
+var playerHand;
 
 var playerContainer = $('.playerHand');
-
 var dealerContainer = $('.dealerHand');
+
+var playerSum;
+var dealerSum; 
 
 var winner = null; 
 
-var bankroll = 500;
+var bankRoll = 500;
 var bet = 0;
 
 
-// d - dealer wins
-//p - player wins
-
-// t - tie 
-//dBL - dealer blackjack
-//pBL - player blackjack
-
-//pB - player busts
 
 
 $('.hit').on('click', function() {
     grabCard(playerHand);
-    var playerSum = computeHand(playerHand);
+    playerSum = computeHand(playerHand);
     if (playerSum > 21) winner = "pB";
+    else if (playerSum === 21) winner = "pBL"; 
     render();
 });
 $('.stand').on('click', checkWin);
 $('.deal').on('click', function() {
     init();
     dealCard();
+
 }); 
 
-$('.bet').on('click', betMoney);
+$('.chip5').on('click', function(){
+    betMoney(5);
+});
+
+$('.chip25').on('click', function(){
+    betMoney(25);
+});
+
+$('.chip100').on('click', function(){
+    betMoney(100);
+});
+
+$('.reset').on('click', function(){ 
+    bankRoll += bet; 
+    bet -= bet; 
+    $('.betPool').html(bet);
+    $('.bankRoll').html(bankRoll);
+});
+
+
+
 
 
 
@@ -47,23 +62,21 @@ function createDeck() {
         {face:'dA',value: 11}, {face:'dK', value: 10},{face:'dQ', value: 10},{face:'dJ', value: 10},{face:'d10', value: 10},{face:'d09', value: 9},{face:'d08', value: 8},{face:'d07', value: 7},{face:'d06', value: 6},{face:'d05', value: 5},{face:'d04', value: 4},{face:'d03', value: 3}, {face:'d02', value: 2}, {face:'hA', value: 11}, {face:'hK', value: 10}, {face:'hQ', value: 10}, {face:'hJ', value: 10}, {face:'h10', value: 10},{face:'h09', value: 9},{face:'h08', value: 8},{face:'h07', value: 7},{face:'h06', value: 6},{face:'h05', value: 5},{face:'h04', value: 4},{face:'h03', value: 3},{face:'h02', value: 2}, {face:'sA', value: 11}, {face:'sK', value: 10}, {face:'sQ', value: 10}, {face:'sJ', value: 10}, {face:'s10', value: 10},{face:'s09', value: 9},{face:'s08', value: 8},{face:'s07', value: 7},{face:'s06', value: 6},{face:'s05', value: 5},{face:'s04', value: 4},{face:'s03', value: 3},{face:'s02', value: 2}, {face: 'cA', value: 11}, {face: 'cK', value: 10}, {face: 'cQ', value: 10}, {face:'cJ', value: 10}, {face:'c10', value: 10},{face:'c09', value: 9},{face:'c08', value: 8},{face:'c07', value: 7},{face:'c06', value: 6}, {face:'c05', value: 5},{face:'c04', value: 4},{face:'c03', value: 3},{face:'c02', value: 2}];
   };
 
+  function betMoney(value){
+    parseInt(value);
+    bet += value;
+    
+    bankRoll -= bet;
+    $('.bankRoll').html(bankRoll); 
+    $('.betPool').html(bet); 
+};
+
 
 function grabCard(hand) {
   var randomNum = Math.floor(Math.random() * deck.length);
   var topCard = deck.splice([randomNum], 1)[0];
   hand.push(topCard);
 };
-
-
-
-function betMoney(){
-    value = parseInt($("input[type='number']").val()); 
-    bet += value;
-};
-
-
-
-
 
 function dealCard (){
     var randomNum1 = Math.floor(Math.random() * deck.length - 1);
@@ -105,14 +118,12 @@ function computeHand(hand) {
     return handSum;
 }
 
-
 function checkWin() {
-    var playerSum = computeHand(playerHand);
-    var dealerSum = computeHand(dealerHand);
+    playerSum = computeHand(playerHand);
+    dealerSum = computeHand(dealerHand);
     while (dealerSum < 17){
         grabCard(dealerHand);
         dealerSum = computeHand(dealerHand);
-        checkWin();
     } 
     if (playerSum > dealerSum && playerSum < 21 || dealerSum > 21){
         winner = "p";
@@ -133,7 +144,6 @@ function checkWin() {
 
 };
 
-
 function render() {
         var className, cardEl;
         playerContainer.html('');
@@ -150,8 +160,35 @@ function render() {
             dealerContainer.append(cardEl);
         });
 
-        winner === null && playerHand.length ? $('.deal').hide() : $('.deal').show();
-        
+        if (winner === null && playerHand.length){
+            $('.deal').hide();
+        } else {
+            $('.deal').show(); 
+        }
+
+        // winner === null && playerHand.length ? $('.deal').hide() : $('.deal').show();
+
+        if (playerHand.length){
+            $('.hit').show();
+            $('.stand').show(); 
+        } 
+
+        if($('.deal').is(":hidden")){
+            $('.chip5').hide();
+            $('.chip25').hide();
+            $('.chip100').hide();
+            $('.reset').hide(); 
+        } else {
+            $('.chip5').show();
+            $('.chip25').show();
+            $('.chip100').show();
+            $('.reset').show(); 
+        }
+
+        if (winner !== null){
+            $('.hit').hide();
+            $('.stand').hide(); 
+        }
 
         switch (winner){
             case "p":
@@ -173,22 +210,20 @@ function render() {
                 $('h3').empty().text("You Busted! You lose!");
                 break;
             default: 
-                 $('h3').empty().text("Wecome to Blackjack. Good luck!");
+                 $('h3').empty().text("Good luck!");
             break; 
         }
     }
 
-
-
-
 function init() {
     playerHand = [];
     dealerHand = [];
+    playerSum = 0;
+    dealerSum = 0; 
     winner = null;
     createDeck(); 
     render();
 };
-
 
 init(); 
 
